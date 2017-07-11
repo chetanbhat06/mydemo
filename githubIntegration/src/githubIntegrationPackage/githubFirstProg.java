a5e5cdf480909efbf10c97c151527e7418fb5771
chetanbhat06 <cjbhat@yahoo.com>
2017-07-11 23:04:16 +0530

    build

--- githubIntegration/src/githubIntegrationPackage/githubFirstProg.java
+++ githubIntegration/src/githubIntegrationPackage/githubFirstProg.java
@@ -1,12 +1,10 @@
 package githubIntegrationPackage;
 
-import org.testng.annotations.Test;
-
 public class githubFirstProg {
 
-	@Test
-	public void gitTest()
-	{
+	
+	public static void main(String[] args) {
+		
 		System.out.println("This is git hub demo with TestnG Jenkins CI");
 	}
 }
