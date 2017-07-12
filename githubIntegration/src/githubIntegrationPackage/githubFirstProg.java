dec2bdcbc6d6b100126b818b342a10d17eadbbca
chetanbhat06 <cjbhat@yahoo.com>
2017-07-12 22:31:25 +0530

    UI steps added

--- githubIntegration/src/githubIntegrationPackage/githubFirstProg.java
+++ githubIntegration/src/githubIntegrationPackage/githubFirstProg.java
@@ -1,10 +1,24 @@
 package githubIntegrationPackage;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class githubFirstProg {
 
 	
 	public static void main(String[] args) {
 		
+		WebDriver driver;
+		String baseUrl;
+		  
+		  
 		System.out.println("This is git hub demo with Jenkins CI");
+		
+		 driver = new FirefoxDriver();
+		 baseUrl = "http://www.Google.com/";
+		 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
 	}
 }
