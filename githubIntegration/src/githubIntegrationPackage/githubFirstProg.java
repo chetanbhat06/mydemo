cbeb8003edbaddc4e1a42e0c133534637d9d953c
chetanbhat06 <cjbhat@yahoo.com>
2017-07-12 22:23:35 +0530

    Ant build.xml

--- githubIntegration/src/githubIntegrationPackage/githubFirstProg.java
+++ githubIntegration/src/githubIntegrationPackage/githubFirstProg.java
@@ -5,6 +5,6 @@ public class githubFirstProg {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("This is git hub demo with TestnG Jenkins CI");
+		System.out.println("This is git hub demo with Jenkins CI");
 	}
 }
